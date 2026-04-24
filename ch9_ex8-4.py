745e5dd9d3b3a247ac135675f22c376e6ab79440
soxtox <soxtox@gmail.com>
2026-04-24 18:43:35 +0545

    Add script to read file and list and sort unique words
    Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in python sort() order as shown in the desired output.
    You can download the sample data at http://www.py4e.com/code3/romeo.txt

---
+++ ch9_ex8-4.py
@@ -0,0 +1,13 @@
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line = line.strip()
+    word = line.split(" ") 
+    for i in word:
+        if i not in lst:
+            lst.append(i)
+    
+lst.sort()
+    
+print(lst)
